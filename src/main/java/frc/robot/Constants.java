d66ba7464d1c2fa591abdcb72aab5514e2a4b404
Team4239 <warpspeed@willmar.org>
2020-01-05 03:23:01 -0600

    Created Some Mock Subsystems and RobotMaping

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,19 +1,13 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants.  This class should not be used for any other purpose.  All constants should be
- * declared globally (i.e. public static).  Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
 public final class Constants {
+    public static final int DRIVETRAIN_LEFT_MOTOR_ONE = 0,    //PDP #0 //TalonFX
+                            DRIVETRAIN_LEFT_MOTOR_TWO = 1,    //PDP #1
+                            DRIVETRAIN_LEFT_MOTOR_THREE = 2,  //PDP #2
+                            DRIVETRAIN_RIGHT_MOTOR_ONE = 3,   //PDP #15
+                            DRIVETRAIN_RIGHT_MOTOR_TWO = 4,   //PDP #14
+                            DRIVETRAIN_RIGHT_MOTOR_THREE = 5; //PDP #13
+
+    public static final int DRIVETRAIN_SHIFTING_SOLENOID_FORWARD = 0, //PCM
+                            DRIVETRAIN_SHIFTING_SOLENOID_REVERSE = 1;
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,7 +2,14 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.DrivetrainShifting;
+import frc.robot.subsystems.Feeder;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.PanelSpinner;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Turret;
 
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -12,7 +19,14 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  //private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final Climber m_climber = new Climber();
+  private final Drivetrain m_drivetrain = new Drivetrain();
+  private final DrivetrainShifting m_drivetrainShifting = new DrivetrainShifting();
+  private final Feeder m_feeder = new Feeder();
+  private final Intake m_intake = new Intake();
+  private final PanelSpinner m_panelSpinner = new PanelSpinner();
+  private final Shooter m_shooter = new Shooter();
+  private final Turret m_turret = new Turret();
 
   //private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
@@ -22,7 +36,7 @@ public class RobotContainer {
    * The container for the robot.  Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
-    // Configure the button bindings
+    
     configureButtonBindings();
   }
 
@@ -41,8 +55,8 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  /*public Command getAutonomousCommand() {
+  public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
     return m_autoCommand;
-  }*/
+  }
 }
