a84f9aecc72c01d1a2288ab0a616ced53c3f5548
Team4239 <warpspeed@willmar.org>
2020-02-07 14:33:21 -0600

    Added motor setup to Turret.java

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,7 +12,11 @@ public class Turret extends SubsystemBase {
   private WPI_TalonSRX motor = new WPI_TalonSRX(Constants.TURRET_MOTOR);
   
   public Turret() {
-
+    motor.configFactoryDefault();
+    motor.setInverted(false);
+    motor.setNeutralMode(NeutralMode.Brake);
+    motor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
+    motor.setSensorPhase(false);
   }
 
   @Override
