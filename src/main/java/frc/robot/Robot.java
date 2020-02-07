a46776d948d75a936f9825c5ac83d6eaf9e18c8a
Team4239 <warpspeed@willmar.org>
2020-02-07 14:30:46 -0600

    Added base for gameData

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,10 +1,12 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
+  private String gameData;
   private Command mAutonomousCommand;
 
   private RobotContainer mRobotContainer;
@@ -52,6 +54,26 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
+    gameData = DriverStation.getInstance().getGameSpecificMessage();
+    if (gameData.length() > 0) {
+      switch (gameData.charAt(0)) {
+        case 'B':
+          //Blue Code
+          break;
+        case 'G':
+          //Green Code
+          break;
+        case 'R':
+          //Red Code
+          break;
+        case 'Y':
+          //Yellow Code
+          break;
+        default:
+        //Unknown/Error
+          break;
+      }
+    }
   }
 
   @Override
